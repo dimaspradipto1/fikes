81370953fa604a80008b9c56ebf94b263d40a697
dimas pradipto <dpradipto8@gmail.com>
2026-06-23 19:37:37 +0700

    feat: import project assets including vendor libraries and update layout and route configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('login', 'login')->name('login');
 });
