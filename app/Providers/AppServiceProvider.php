8e4bf3664f0246d9624fc89ac73f47b439845f48
dimas pradipto <dpradipto8@gmail.com>
2026-06-23 19:07:00 +0700

    feat: scaffold dashboard and user controllers, role middleware, and app service provider configurations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setLocale('id');
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
